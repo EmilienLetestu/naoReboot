b25c76268f6f6cdd5489241345276b82c67fdbd0
emilien letestu <eletestu@gmail.com>
2017-09-26 18:12:18 +0200

    [add] fully functional registration system

---
+++ src/Services/Register.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 26/09/17
+ * Time: 14:47
+ */
+
+namespace App\Services;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+class Register
+{
+    private $formFactory;
+    private $requestStack;
+    private $doctrine;
+    private $swift;
+    private $mailService;
+    private $tools;
+    private $session;
+
+    public  function __construct(
+        FormFactory         $formFactory,
+        RequestStack        $requestStack,
+        EntityManager       $doctrine,
+        \Swift_Mailer       $swift,
+        Mails               $mailService,
+        Tools               $tools,
+        Session             $session
+
+
+    )
+    {
+        $this->formFactory  = $formFactory;
+        $this->requestStack = $requestStack;
+        $this->doctrine     = $doctrine;
+        $this->swift        = $swift;
+        $this->mailService  = $mailService;
+        $this->tools        = $tools;
+        $this->session      = $session;
+    }
+
+
+    /**
+     * @param Request $request
+     * @return array|string
+     */
+    public function registerUser(Request $request)
+    {
+        $user = new User();
+        $registerForm = $this->formFactory->create(RegisterType::class, $user);
+        $date = date('Y-m-d');
+
+        $registerForm->handleRequest($request);
+        if($registerForm->isSubmitted() && $registerForm->isValid())
+        {
+            //hydrate with submitted data
+            $user->setName($registerForm->get('name')->getData());
+            $user->setSurname($registerForm->get('surname')->getData());
+            $user->setEmail($registerForm->get('email')->getData());
+            $user->setPswd($registerForm->get('pswd')->getData());
+            $user->setCreatedOn(\DateTime::createFromFormat('Y-m-d', $date));
+            $user->setAccessLevel($registerForm->get('accessLevel')->getData());
+
+            $emailInDb = $this->mailService
+                ->checkMailAvailability($user->getEmail())
+            ;
+
+            if ($emailInDb !== null)
+            {
+                $this->session->getFlashBag()
+                    ->add('denied',
+                        'Cette email est déjà utilisé'
+                    )
+                ;
+                return $render = [$registerForm->createView()];
+            }
+
+            //check if user requested an access level 1 or 2
+            $status = $this->tools->getUserAccountStatus($user->getAccessLevel());
+
+            //hydrate with default value
+            $user->setOnHold($status);
+            $user->setActivated(false);
+            $user->setConfirmationToken(40);
+            $user->setBan(false);
+            $user->setDeactivated(false);
+
+            //prepare email
+            $message = $this->mailService->validationMail(
+                $user->getName(),
+                $user->getSurname(),
+                $user->getConfirmationToken(),
+                $user->getEmail()
+            );
+
+            //save
+            $this->doctrine->persist($user);
+            $this->doctrine->flush();
+
+            //send validation email
+            $this->swift->send($message);
+
+            return $redirect = 'home';
+        }
+
+        return $render = [$registerForm->createView()];
+    }
+}
