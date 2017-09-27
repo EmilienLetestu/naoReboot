56b3f479b977283a9ada7bc7316aa9bf6697ede6
emilien letestu <eletestu@gmail.com>
2017-09-27 09:43:01 +0200

    [add] forms for password lost/change

---
+++ src/Form/AskResetType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 27/09/17
+ * Time: 09:11
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class AskResetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       $builder
+           ->add('email', EmailType::class, ['constraints' => [new NotBlank(),
+                                                               new Email([
+                                                                   'message' => 'Ceci n\'est pas un email valide'])
+                                                               ]
+           ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+       return $resolver->setDefaults(['data-class' =>'App\Entity\User']);
+    }
+
+}
---
+++ src/Form/ResetPswdType.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 27/09/17
+ * Time: 09:28
+ */
+
+namespace App\Form;
+
+
+use App\Validators\PswdFormat;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+
+class ResetPswdType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pswd', PasswordType::class, ['constraints' => [new NotBlank(),
+                                                                  new PswdFormat(),
+                                                                  new Type('string'),
+                                                                  new Length([
+                                                                    'min'        => 6,
+                                                                    'max'        => 30,
+                                                                    'minMessage' => 'Le mot de passe doit être composé de 6 à 30 caractères',
+                                                                    'maxMessage' => 'Le mot de passe doit être composé de 6 à 30 caractères'])
+                                                                  ],
+                                                 'label' => 'Nouveau mot de passe'
+            ])
+            ->add('confirmPswd', TextType::class, ['constraints' => [new NotBlank(),
+                                                                     new Type('string')
+                                                                    ],
+                                                   'required' => true,
+                                                   'mapped'   => false,
+                                                   'label'    => 'Confirmer le mot de passe'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+       return $resolver->setDefaults(['data-class', 'App\Entity\User']);
+    }
+
+}
