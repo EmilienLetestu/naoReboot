5df3552e1e202828e26ec70d454ca5b68561d39a
emilien letestu <eletestu@gmail.com>
2017-09-29 11:24:11 +0200

    [add] controllers  -> temporary will use action later on

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 23/09/17
+ * Time: 22:45
+ */
+
+namespace App\Controller;
+
+use App\Form\Login;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class IndexController extends Controller
+{
+    public function home()
+    {
+        $this->get('session')->set('sdsqdq','hello');
+
+        return $this->render('home.html.twig');
+    }
+
+    public function login(Request $request)
+    {
+        $view = $this->get('form.factory')->create(Login::class);
+       dump($request->getSession()->get('sdsqdq'));
+        return $this->render(
+            'connectionForms.html.twig',
+            ['form' => $view->createView()]
+        );
+    }
+
+    public function register(Request $request)
+    {
+        $view = $this->get('App\Services\Register')->registerUser($request);
+        if($view === 'home')
+        {
+            return $this->redirectToRoute($view);
+        }
+        return $this->render(
+            'connectionForms.html.twig',
+            ['form' => $view[0]]
+        );
+    }
+
+    public function activate(Request $request)
+    {
+        $this->get('App\Services\ActivateAccount')->ActivateUserAccount($request);
+
+        return $this->redirectToRoute('home');
+    }
+
+    public function resetPswdMail(Request $request)
+    {
+        $view = $this->get('App\Services\ResetPswd')->askReset($request);
+
+        return $this->render(
+            'connectionForms.html.twig',
+            ['form'=>$view]
+        );
+    }
+
+    public function resetPswdForm(Request $request)
+    {
+        $view = $this->get('App\Services\ResetPswd')->resetPswd($request);
+
+        if($view === 'home')
+        {
+            return $this->redirectToRoute($view);
+        }
+        return $this->render(
+            'connectionForms.html.twig',
+            ['form'=> $view]
+        );
+    }
+
+    public function addReport(Request $request)
+    {
+        $view = $this->get('App\Services\AddReport')->addReportProcess($request);
+
+        return $this->render(
+            'addReportForm.html.twig',
+            ['form' => $view]
+        );
+    }
+
+}
