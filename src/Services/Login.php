4560d70cbce2040077ce45e4cb738dc1b8152d98
emil <eletestu@gmail.com>
2017-10-03 16:25:31 +0200

    [add] new function to store user previous activities on session variables

--- src/Services/Login.php
+++ src/Services/Login.php
@@ -8,22 +8,43 @@
 
 namespace App\Services;
 
+use App\Entity\Star;
+use App\Entity\User;
+use App\Entity\Validation;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class Login
 {
-    private $security;
+    private $authCheck;
+    private $session;
+    private $token;
+    private $doctrine;
 
     /**
      * Login constructor.
-     * @param AuthorizationChecker $security
+     * @param AuthorizationChecker $authCheck
+     * @param Session $session
+     * @param TokenStorage $token
+     * @param EntityManager $doctrine
      */
-    public function  __construct(AuthorizationChecker $security)
+    public function  __construct(
+        AuthorizationChecker $authCheck,
+        Session              $session,
+        TokenStorage         $token,
+        EntityManager        $doctrine
+    )
     {
-        $this->security  = $security;
+        $this->authCheck  = $authCheck;
+        $this->session    = $session;
+        $this->token      = $token;
+        $this->doctrine   = $doctrine;
     }
 
     /**
@@ -33,8 +54,8 @@ class Login
      */
     public function processLogin(Request $request,AuthenticationUtils $authUtils)
     {
-        if ( $this->security->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-
+        if ( $this->authCheck->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+        {
             return $redirect = 'home';
         }
 
@@ -44,10 +65,37 @@ class Login
         // last username entered by the user
         $lastUsername = $authUtils->getLastUsername();
 
+
         return [
             $lastUsername,
             $error]
         ;
+    }
 
+    /**
+     * fetch all stars and validations added by user,
+     * store them into session
+     * @return null
+     */
+    public function getUserHistory()
+    {
+        if(!$this->authCheck->isGranted('ROLE_USER') || $this->session->has('star'))
+        {
+
+            return null;
+        }
+        $user = $this->token->getToken()->getUser();
+        $star = $this->doctrine->getRepository(Star::class)
+            ->findStarAddedBy($user->getId());
+        ;
+        $this->session->set('star', $star);
+
+        if($this->authCheck->isGranted('ROLE_VALIDATOR'))
+        {
+            $validation = $this->doctrine->getRepository(Validation::class)
+                ->findValidationAddedBy($user->getId())
+            ;
+            $this->session->set('validation', $validation);
+        }
     }
 }
