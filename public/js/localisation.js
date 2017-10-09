1f2c3829406b1ccc4db0f9feb220a97bfc179bac
emil <eletestu@gmail.com>
2017-10-10 01:51:01 +0200

    [mod!-- works on chrome not on firefox --!] some refactoring

--- public/js/localisation.js
+++ public/js/localisation.js
@@ -1,77 +1,62 @@
 var geocoder;
-var btn = document.getElementById('localizeMe');
 
+var btn = document.getElementById("localize");
+var address = document.getElementById("report_location").value;
 
-// Get the latitude and the longitude;
-function successFunction(position) {
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    codeLatLng(lat, lng);
-}
-
-// Get Error
-function errorFunction() {
-    alert('Echec de la géolocalisation');
-}
 
 //initialize Google Map
 function initMap() {
     geocoder = new google.maps.Geocoder();
-
 }
 
 //auto find lat, lng and address
-function codeLatLng(lat, lng) {
+function getAll() {
+    navigator.geolocation.getCurrentPosition(function (position) {
 
-    var latLng = new google.maps.LatLng(lat, lng);
-    geocoder.geocode({'latLng': latLng}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results)
-            if (results[1]) {
-                //find county name
-                for (var i=0; i<results[0].address_components.length; i++) {
-                    for (var b=0;b<results[0].address_components[i].types.length;b++) {
-                        //try to find : administrative area level 2
-                        if (results[0].address_components[i].types[b] == 'administrative_area_level_2') {
-                            county= results[0].address_components[i];
-                            break;
-                        }
-                    }
+        var geocoder = new google.maps.Geocoder();
+        var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        console.log(position.coords.latitude + ', ' + position.coords.longitude);
+
+        geocoder.geocode({'latLng': geolocate}, function (results, status) {
+            if (status === "OK") {
+                var result;
+                if (results.length > 1) {
+                    result = results[1];
+                } else {
+                    result = results[0];
                 }
-                //insert collected data into form
-                document.getElementById('report_location').value = results[0].formatted_address + ','  + county.short_name;
-                document.getElementById('report_satNav').value   = lat + ', ' + lng
-            } else {
-                alert('aucun résultat trouvé');
+                //console.log(result);
+                console.log(result.address_components[0].long_name + ', ' + result.address_components[2].long_name);
+                document.getElementById("report_location").value = result.address_components[0].long_name + ', ' + result.address_components[2].long_name;
+                document.getElementById("report_satNav").value = position.coords.latitude + ', ' + position.coords.longitude;
             }
-        } else {
-            alert('Echec réseau: ' + status);
-        }
+            if (!results) {
+                alert('dsddqd');
+            }
+        });
     });
 }
 
 //find lat and lng from submitted location
-function codeLocation(geocoder)
-{
-    var address = document.getElementById('report_location').value;
-    geocoder.geocode({'address': address}, function (results, status) {
-        if(status === 'OK'){
-            alert('Coordonnées GPS qui seront enregistrées : ' + results[0].geometry.location)
-            document.getElementById('satNav').value = location;
-        }else{
-            alert('Aucune données pour ce lieu !');
-
+function codeLocation(geocoder) {
+    var address = document.getElementById("report_location").value;
+    geocoder.geocode({"address": address}, function (results, status) {
+        if (status === "OK") {
+            alert("Coordonnées GPS qui seront enregistrées : " + results[0].geometry.location);
+            document.getElementById("report_satNav").value = location;
+        } else {
+            alert("Aucune données pour ce lieu !");
         }
-
     });
 }
 
-btn.onclick = function localizedMe () {
-    var address = document.getElementById('report_location').value;
-    if (navigator.geolocation && address === '' ) {
-        navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-    }
-    if(address !== ''){
+function localizeMe() {
+    var address = document.getElementById("report_location").value;
+    if (address !== "") {
         codeLocation(geocoder);
+    }
+    else {
+        getAll();
     }
 }
