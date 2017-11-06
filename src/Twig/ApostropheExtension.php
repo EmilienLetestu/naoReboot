53e8f17ea795c88ffbba552f82ad1c90d9408ce3
emilien_letestu <eletestu@gmail.com>
2017-11-06 18:21:51 +0100

    [add]|[twig filter] create new custom twig filter

---
+++ src/Twig/ApostropheExtension.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 06/11/2017
+ * Time: 17:44
+ */
+
+namespace App\Twig;
+
+
+class ApostropheExtension extends \Twig_Extension
+{
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('apostrophe',[$this, 'vowelFilter'])
+        ];
+    }
+
+
+    /**
+     * @param $birdName
+     * @return string
+     */
+    public function vowelFilter($birdName)
+    {
+        $vowel = ['a','e','i','o','u','y'];
+        
+        return in_array(strtolower($birdName[0]),$vowel) ? 'd\''.$birdName:'de '.$birdName;
+    }
+}
