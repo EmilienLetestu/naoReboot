92c9f5fe2f2b4dadfd6d1ef8318d90994effc24a
emilien_letestu <eletestu@gmail.com>
2017-11-13 12:47:54 +0100

    [add]|[twig filter] trun notification type into its matching description

---
+++ src/Twig/NotificationTypeExtension.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 13/11/2017
+ * Time: 12:04
+ */
+
+namespace App\Twig;
+
+
+class NotificationTypeExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('typeToText',[$this,'notificationFilter'])
+        ];
+}
+
+    public function notificationFilter($type)
+    {
+        $list = [
+            '1'=>'Votre niveau de privilège a changé, vous disposez maintenant d\'un compte amateur',
+            '2'=>'Félicitations votre compte à été promu, vous disposez à présent d\'un compte naturalise!',
+            '3'=>'Votre demande de compte naturaliste à été accepté',
+            '4'=>'Votre demande de compte naturaliste à été refusé']
+            ;
+
+            return $list[$type];
+
+    }
+}
