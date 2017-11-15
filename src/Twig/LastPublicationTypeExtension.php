8e5f3c62de7e52e9368e7577fe279e6144bc6457
emilien_letestu <eletestu@gmail.com>
2017-11-15 14:04:34 +0100

    [add]|[twig filter] set of custom filters

---
+++ src/Twig/LastPublicationTypeExtension.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 15/11/2017
+ * Time: 13:22
+ */
+
+namespace App\Twig;
+
+use App\Services\ActivitiesTracker;
+
+class LastPublicationTypeExtension extends \Twig_Extension
+{
+    private  $activitiesTracker;
+
+    public function __construct(
+        ActivitiesTracker $activitiesTracker
+    )
+    {
+        $this->activitiesTracker = $activitiesTracker;
+    }
+
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('lastPublication',[$this,'lastPublicationFilter'])
+        ];
+    }
+
+    public function lastPublicationFilter($id)
+    {
+        $lastPublication = $this->activitiesTracker->getLastPublicationData($id);
+
+        return [
+            'date'   => $lastPublication[0],
+            'bird'   => $lastPublication[1],
+            'satNav' => $lastPublication[2]
+        ];
+    }
+}
