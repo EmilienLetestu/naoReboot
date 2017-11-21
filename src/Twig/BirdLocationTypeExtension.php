7640786bbb67e0a359f1ead1747cfd7be915eea1
emilien_letestu <eletestu@gmail.com>
2017-11-21 17:17:16 +0100

    [add]|[Twig extension] fetch report based on bird id

---
+++ src/Twig/BirdLocationTypeExtension.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 21/11/2017
+ * Time: 16:03
+ */
+
+namespace App\Twig;
+
+
+use App\Entity\Report;
+use Doctrine\ORM\EntityManager;
+
+class BirdLocationTypeExtension extends \Twig_Extension
+{
+    private $doctrine;
+
+    public function __construct(
+        EntityManager $doctrine
+    )
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('birdLocation',[$this,'birdLocationFilter'])
+        ];
+    }
+
+    public function birdLocationFilter($birdId)
+    {
+        $repository = $this->doctrine->getRepository(Report::class);
+
+       return $report = $repository->findByBird($birdId);
+    }
+}
