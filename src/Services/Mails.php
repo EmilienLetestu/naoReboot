58c1b61203b4f82f802105e72e95b71c8ea6ca9f
emilien_letestu <eletestu@gmail.com>
2017-11-28 16:00:00 +0100

    [fix]|[mail] fix unreceived mail bug (+ enable ssl on local server)

--- src/Services/Mails.php
+++ src/Services/Mails.php
@@ -55,7 +55,7 @@ class Mails
     {
         $message = (new \Swift_Message('Activation de votre compte Nao'));
         $message
-            ->setFrom('validation@nao.fr')
+            ->setFrom('eltestu@gmail.com')
             ->setTo($email)
             ->setBody($this->twig->render('validationMail.html.twig', [
                 'name'     => $name,
@@ -64,7 +64,9 @@ class Mails
                 'email'    => $email,
                 'expireOn' => date('Y-m-d', strtotime('+2 day'))
             ]),
-                'text/html');
+            'text/html'
+            )
+        ;
         return $message;
     }
 
