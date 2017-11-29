378bd0130bb11d2f796305ef07f4a9d996b2ee82
emilien_letestu <eletestu@gmail.com>
2017-11-29 17:09:49 +0100

    [add]|[form] form to filter unvalidated report

---
+++ src/Form/UnvalidatedFilterType.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 29/11/2017
+ * Time: 16:49
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use App\Repository\ReportRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UnvalidatedFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('order', ChoiceType::class,[
+                                'choices' => [  'Les plus récentes'  => 1,
+                                'les plus anciennes' => 2,
+                                'A-Z'                => 3
+                ],
+                'placeholder' => 'Ordre d\'affichage',
+                'required'    => false,
+                'mapped'      => false
+            ])
+            ->add('bird', EntityType::class, [
+                                'class' => 'App:Report',
+                                'choice_label'  => 'bird.getSpeciesForForm',
+                                'choice_value'  => 'bird.id',
+                                'placeholder'   => 'Rechercher une espèce',
+                                'required'      => false,
+                                'query_builder' => function(ReportRepository $repository){
+                                    return $repository->findSpeciesForForm($validated = 0);
+                                }
+            ])
+        ;
+    }
+}
+
+
