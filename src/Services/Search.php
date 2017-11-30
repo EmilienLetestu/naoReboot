c72c777ccd12e949f937566ad2d1db5998b9c010
emilien_letestu <eletestu@gmail.com>
2017-11-30 12:13:09 +0100

    [add]|[service] fully functionnal searching service

--- src/Services/Search.php
+++ src/Services/Search.php
@@ -8,6 +8,7 @@
 
 namespace App\Services;
 
+use App\Entity\Bird;
 use App\Form\NavSearchType;
 use App\Managers\ReportManager;
 use Doctrine\ORM\EntityManager;
@@ -19,11 +20,16 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 class Search
 {
     private $formFactory;
+    private $doctrine;
 
 
-    public function __construct(FormFactory  $formFactory)
+    public function __construct(
+        FormFactory   $formFactory,
+        EntityManager $doctrine
+    )
     {
-        $this->formFactory       = $formFactory;
+        $this->formFactory  = $formFactory;
+        $this->doctrine     = $doctrine;
     }
 
     public function processSearch(Request $request)
@@ -34,7 +40,21 @@ class Search
         if($searchForm->isSubmitted() && $searchForm->isValid())
         {
 
+          $repository = $this->doctrine->getRepository(Bird::class);
+
+           return[
+               $repository->findBirdLike($searchForm
+                   ->get('search')->getData()
+               ),
+               $searchForm->get('search')->getData()
+           ];
         }
+
+    }
+
+    public function createSearch()
+    {
+        $searchForm = $this->formFactory->create(NavSearchType::class);
 
         return $searchForm->createView();
     }
