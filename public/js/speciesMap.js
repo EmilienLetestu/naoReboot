1178702f7de2b96cccfb136c1d8b327402debf3f
emilien_letestu <eletestu@gmail.com>
2017-11-30 16:55:48 +0100

    [mod]|[gmap Api] modify script to fit new logic

---
+++ public/js/speciesMap.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Emilien on 21/11/2017.
+ */
+
+    function initMap() {
+
+    //bird map
+    var center = new google.maps.LatLng(46.7667, 2.45);
+    var birdLocations = document.getElementsByClassName('locations');
+    var birdMap = new google.maps.Map(document.getElementById('map'), {
+        zoom: 5,
+        center:  center
+
+    });
+
+    var markers = [];
+    for( var i = 0; i < birdLocations.length; i++)
+    {
+        var birdData   = birdLocations[i].value.split(',');
+        var birdLatLng = new google.maps.LatLng(birdData[0], birdData[1]);
+
+        var birdMarker = new google.maps.Marker({
+            position: birdLatLng,
+            map:birdMap
+        });
+        markers.push(birdMarker);
+    }
+}
+
