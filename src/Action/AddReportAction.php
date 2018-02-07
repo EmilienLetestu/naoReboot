18af6a2a056b4d4683c9c5c6efa4ca17c37cf6e5
emilien_letestu <eletestu@gmail.com>
2018-02-07 09:12:13 +0100

    [mod/action] use ReportHandler

--- src/Action/AddReportAction.php
+++ src/Action/AddReportAction.php
@@ -10,6 +10,7 @@ namespace App\Action;
 
 use App\Entity\Report;
 use App\Form\ReportType;
+use App\Handler\ReportHandler;
 use App\Responder\AddReportResponder;
 use App\Services\Tools;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,96 +22,63 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 
 class AddReportAction
 {
+    /**
+     * @var FormFactoryInterface
+     */
     private $formFactory;
+
+    /**
+     * @var EntityManagerInterface
+     */
     private $doctrine;
-    private $session;
-    private $tools;
-    private $token;
+
+    /**
+     * @var ReportHandler
+     */
+    private $reportHandler;
 
     /**
      * AddReportAction constructor.
      * @param FormFactoryInterface $formFactory
      * @param EntityManagerInterface $doctrine
-     * @param SessionInterface $session
-     * @param Tools $tools
-     * @param TokenStorageInterface $token
+     * @param ReportHandler $reportHandler
      */
     public function __construct(
         FormFactoryInterface   $formFactory,
         EntityManagerInterface $doctrine,
-        SessionInterface       $session,
-        Tools                  $tools,
-        TokenStorageInterface  $token
-
+        ReportHandler          $reportHandler
     )
     {
-        $this->formFactory  = $formFactory;
-        $this->doctrine     = $doctrine;
-        $this->session      = $session;
-        $this->tools        = $tools;
-        $this->token        = $token;
+        $this->formFactory   = $formFactory;
+        $this->doctrine      = $doctrine;
+        $this->reportHandler = $reportHandler;
     }
 
+    /**
+     * @param Request $request
+     * @param AddReportResponder $responder
+     * @return RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function __invoke(Request $request, AddReportResponder $responder)
     {
-        //generate form and required object
         $report = new Report();
-        $reportForm = $this->formFactory->create(ReportType::class, $report);
+        $form = $this->formFactory
+                     ->create(ReportType::class, $report)
+                     ->handleRequest($request)
+        ;
 
-        $reportForm->handleRequest($request);
+        $handler = $this->reportHandler->handle($form, $report);
 
-        //process form
-        if($reportForm->isSubmitted() && $reportForm->isValid())
+        if($handler)
         {
-            //get user level and object
-            $level   = $this->token->getToken()->getUser()->getAccessLevel();
-            $onHold  = $this->token->getToken()->getUser()->getOnHold();
-            $user    = $this->token->getToken()->getUser();
-
-            //prepare report default data
-            $default = $this->tools->reportGateways($level,$onHold);
-
-            //hydrate report object
-            $report
-                ->setValidated($default[0])
-                ->setValidationScore($default[1])
-                ->setStarNbr($default[2])
-                ->setUser($user);
-            if($reportForm->get('comment')->getData())
-            {
-                $report->setComment($reportForm->get('comment')->getData());
-            }
-            //----process pict----//
-            //--1 generate filename
-            $birdSpecies = serialize($reportForm->get('bird')->getData());
-            //--2 get submitted file
-            $file = $reportForm->get('pictRef')->getData();
-
-            if($file !== null)
-            {
-                $pictName = $this->tools->generateDataForUserImg(
-                    $birdSpecies,
-                    $user->getId()
-                );
-                //--3 rename it and store it into userImages directory
-                $file->move(
-                    '../public/userImages',
-                    $filename = "$pictName[0].{$file->guessExtension()}"
-                );
-                //--4 hydrate report with filename
-                $report->setPictRef($filename);
-            }
-
-            //save
             $this->doctrine->getRepository(Report::class);
             $this->doctrine->persist($report);
             $this->doctrine->flush();
 
-            $this->session->getFlashBag()->add('success','Votre observation a bien été ajoutée !');
-
             return new RedirectResponse('/observations/nouvelle-observation');
         }
 
-        return $responder($reportForm->createView());
+        return $responder($form->createView());
     }
 }
+
