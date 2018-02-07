67e19fcce39630d288b560334f234bba147df6ac
emilien_letestu <eletestu@gmail.com>
2018-02-07 15:03:01 +0100

    [mod] remove updateHome form handling from homeImg service

--- src/Services/HomeImg.php
+++ src/Services/HomeImg.php
@@ -8,73 +8,8 @@
 
 namespace App\Services;
 
-use App\Form\UpdateHomeType;
-
-use Symfony\Component\Form\FormFactory;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
-
 class HomeImg
 {
-    private $formFactory;
-    private $tools;
-    private $session;
-
-    /**
-     * Admin constructor.
-     * @param FormFactory $formFactory
-     * @param Tools $tools
-     * @param Session $session
-     */
-    public function __construct(
-        FormFactory   $formFactory,
-        Tools         $tools,
-        Session       $session
-    )
-    {
-        $this->formFactory  = $formFactory;
-        $this->tools        = $tools;
-        $this->session      = $session;
-    }
-
-    /**
-     * @param Request $request
-     * @return string|\Symfony\Component\Form\FormView
-     */
-    public function addPictureToHomePage(Request $request)
-    {
-        $updateForm = $this->formFactory->create(UpdateHomeType::class);
-
-        $updateForm->handleRequest($request);
-        if($updateForm->isSubmitted() && $updateForm->isValid())
-        {
-            //process data
-            $birdSpecies  = $updateForm->get('species')->getData();
-            $pictToUpdate = $updateForm->get('pictNum')->getData();
-
-            $file         = $updateForm->get('picture')->getData();
-            $pictureData  = $this->tools->generateDataForHomeImg(
-                str_replace(' ','_',$birdSpecies),
-                $pictToUpdate
-            );
-
-            //remove file to update
-            $this->deletePreviousHomeImage($pictToUpdate);
-
-            $file->move(
-                '../public/naoPictures',
-                $pictureData.'.'.$file->guessExtension()
-            );
-
-            $this->session->getFlashBag()
-                ->add('success','Mise à jour effectuée');
-
-            return 'admin';
-        }
-
-        return $updateForm->createView();
-
-    }
 
     /**
      * @return array
