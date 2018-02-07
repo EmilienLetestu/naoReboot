97965a57412f9487960db2672a11e0c56fa371b8
emilien_letestu <eletestu@gmail.com>
2018-02-07 22:52:03 +0100

    [googleMap api] fix => used to display same location on every map before fix

--- public/js/reportMap.js
+++ public/js/reportMap.js
@@ -4,24 +4,22 @@
 function initMap(loopIndex) {
 
     // report maps
-    var locations = document.querySelectorAll('*[id^="reportMapSatNav"]');
+    var getid = 'reportMapSatNav' + loopIndex;
+    var location =  document.getElementById(getid);
+    var data     = location.value.split(',');
+    var latLng = new google.maps.LatLng(data[0], data[1]);
+    var mapId = 'reportMap'+loopIndex;
+    var map = new google.maps.Map(document.getElementById(mapId), {
+        zoom: 6,
+        center: latLng
+    });
 
-    for( var i = 0; i < locations.length; i++ )
-    {
-        var data = locations[i].value.split(',');
-        var latLng = new google.maps.LatLng(data[0], data[1]);
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+    });
 
-        var mapId = 'reportMap'+loopIndex;
 
-        var map = new google.maps.Map(document.getElementById(mapId), {
-            zoom: 6,
-            center: latLng
-        });
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-        });
-    }
     google.maps.event.addListener(map, 'idle', function(){
         google.maps.event.trigger(map, 'resize');
         map.setCenter(latLng);
