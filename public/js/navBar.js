a37112b75dbd0f592744872b1630a6655147880f
emilien_letestu <eletestu@gmail.com>
2018-02-09 14:06:08 +0100

    [add/js] preapare switching from vue.js to native javascript

---
+++ public/js/navBar.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Emilien on 09/02/2018.
+ */
+function searchWidth() {
+
+    var isfocus = document.activeElement;
+
+    if(document.getElementById('nav_search_search') === isfocus){
+        document.getElementById('search').style.width = "600px";
+        document.getElementById('nav_search_submit').style.left="49%";
+    }
+    else{
+        document.getElementById('search').style.width = "inherit";
+        document.getElementById('nav_search_submit').style.left="20%";
+    }
+}
