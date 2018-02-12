d245abf322003f607b6d9a07311ddb326e3eba22
emilien_letestu <eletestu@gmail.com>
2018-02-12 13:07:53 +0100

    [mod/template/js] js function dedicated to show/hide maps + button to hide map + style

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -15,10 +15,32 @@ function searchWidth() {
     }
 }
 
+function getStyle(id, prop) {
+    var element = document.getElementById(id);
+    var cssProp = window.getComputedStyle(element,null).getPropertyValue(prop);
+    if(!cssProp){
+        return false
+    }
+    return cssProp;
+}
+
 function show(id) {
-    document.getElementById(id).style.display = "block";
+
+    document.getElementById(id).style.visibility = "visible";
 }
 
 function hide(id){
-    document.getElementById(id).style.display = "none"
+
+    document.getElementById(id).style.visibility = "hidden";
+}
+
+function showMap(id,buttonId) {
+
+    document.getElementById(id).style.visibility = "visible";
+    document.getElementById(buttonId).style.visibility = "visible";
+}
+
+function hideMap(id,buttonId) {
+    document.getElementById(id).style.visibility = "hidden";
+    document.getElementById(buttonId).style.visibility = "hidden";
 }
