dbe47dd51f9cbb996ff298560f505feb91fa1a66
emilien_letestu <eletestu@gmail.com>
2018-02-12 18:01:33 +0100

    [mod/js] create wordcount()

--- public/js/emilLibrary.js
+++ public/js/emilLibrary.js
@@ -1,6 +1,11 @@
 /**
  * Created by Emilien on 12/02/2018.
  */
+/**
+ * @param element
+ * @param prop
+ * @returns {*}
+ */
 function getStyle(element, prop) {
 
     var cssProp = window.getComputedStyle(element,null).getPropertyValue(prop);
@@ -11,15 +16,35 @@ function getStyle(element, prop) {
     return cssProp;
 }
 
+/**
+ * @param id
+ * @returns {Element}
+ */
 function $id(id) {
     return document.getElementById(id);
 }
 
+/**
+ * @param className
+ * @returns {NodeList}
+ */
 function $class(className) {
    return document.getElementsByClassName(className);
 }
 
+/**
+ * @param element
+ * @param removeClass
+ * @param addClass
+ */
 function swappClass(element,removeClass,addClass){
     element.classList.remove(removeClass);
     element.classList.add(addClass);
+}
+
+/**
+ * @param string
+ */
+function wordCount(string){
+    return string.split(' ').length;
 }
