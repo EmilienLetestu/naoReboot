aa082fa2ecb775ce1ef2992f7db9a9094f040528
emilien_letestu <eletestu@gmail.com>
2018-02-13 12:00:48 +0100

    !! code indent !!

--- public/js/connectionForm.js
+++ public/js/connectionForm.js
@@ -1,20 +1,38 @@
 /**
  * Created by Emilien on 13/02/2018.
  */
+/**
+ * @param id
+ * @param min
+ * @param max
+ * @param errorId
+ * @returns {boolean}
+ */
 function validateText(id,min,max,errorId){
+
     var validate  = true;
     var userInput = $id(id).value.length;
 
     if(userInput < min || userInput > max){
       swappClass($id(errorId),'noError','has-error');
+
       return validate = false;
     }
 
     swappClass($id(errorId),'has-error','noError');
+
     return validate;
 }
 
+/**
+ * @param id
+ * @param min
+ * @param max
+ * @param errorId
+ * @returns {boolean}
+ */
 function validatePswd(id,min,max,errorId){
+
     var validate           = true;
     var pswd               = $id(id).value;
     var mixLetterAndNumber = /^(?=.*[a-zA-z])(?=.*\d)/;
@@ -22,14 +40,22 @@ function validatePswd(id,min,max,errorId){
 
     if(pswd.length < min || pswd.length > max || !mixLetterAndNumber.test(pswd) || series.test(pswd)){
         swappClass($id(errorId),'noError','has-error');
+
         return validate = false;
     }
 
     swappClass($id(errorId),'has-error','noError');
+
     return validate;
 }
 
+/**
+ * @param id
+ * @param errorId
+ * @returns {boolean}
+ */
 function validateMail(id,errorId){
+
     var validate = true;
     var email    = $id(id).value;
     var regex    =   /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
@@ -40,9 +66,13 @@ function validateMail(id,errorId){
     }
 
     swappClass($id(errorId),'has-error','noError');
+
     return validate;
 }
 
+/**
+ * @returns {boolean}
+ */
 function required(){
 
     var validate = true;
@@ -51,6 +81,7 @@ function required(){
     for(var i=0; i < req.length; i++) {
         var val = req[i].value.length;
         if(!req[i].value.length){
+
             return validate = false;
         }
     }
--- public/js/contact.js
+++ public/js/contact.js
@@ -1,34 +1,59 @@
 /**
  * Created by Emilien on 12/02/2018.
  */
+/**
+ * @param id
+ * @param min
+ * @param max
+ * @returns {boolean}
+ */
 function fullname(id,min,max){
+
     var validate = true;
     var userInput = $id(id).value.length;
+
     if(userInput < min || userInput > max){
         swappClass($id('fullnameError'),'noError','has-error');
+
         return validate = false;
     }
+
     swappClass($id('fullnameError'),'has-error','noError');
+
     return validate;
 }
 
+/**
+ * @param id
+ * @param min
+ * @param max
+ * @returns {boolean}
+ */
 function message(id,min,max) {
+
     var validate = true;
     var userInput = $id(id).value;
     var total = wordCount(userInput);
 
     if(total < min || total > max){
         swappClass($id('contactMsgError'),'noError','has-error');
+
         return validate = false;
     }
+
     swappClass($id('contactMsgError'),'has-error','noError');
+
     return validate;
 }
 
+/**
+ * @returns {boolean}
+ */
 function required(){
 
     var validate = true;
     var req = $id('contatcForm').querySelectorAll('[required]');
+
     var userInput = [];
     for(var i=0; i < req.length; i++) {
         var val = req[i].value.length;
@@ -36,6 +61,7 @@ function required(){
             return validate = false;
         }
     }
+
     return validate;
 }
 
