84eb44b915e40f140a14a19537528da7c1cb63e9
emilien_letestu <eletestu@gmail.com>
2018-02-15 12:46:55 +0100

    [add/js] display reports and member on demand

---
+++ public/js/loadMember.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Emilien on 15/02/2018.
+ */
+window.addEventListener('load', function () {
+    loadElement(6,'memberInfo','member');
+});
---
+++ public/js/loadReport.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Emilien on 15/02/2018.
+ */
+/**
+ * @param limit
+ */
+window.addEventListener('load', function () {
+    loadElement(6,'card','reportCard');
+});
