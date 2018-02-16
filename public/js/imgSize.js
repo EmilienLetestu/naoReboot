19010dfbcfd0cce46b6fac3c8248dc83b001d5d4
emilien_letestu <eletestu@gmail.com>
2018-02-16 13:47:04 +0100

    [add/js] adapt css ''objectFit'' property to img size

---
+++ public/js/imgSize.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Emilien on 16/02/2018.
+ */
+
+/***
+ * @param id
+ * @returns {string}
+ */
+function evalSize(id) {
+    var ratio = document.getElementById(id).naturalWidth/document.getElementById(id).naturalHeight;
+    return ratio < 1.1 ? document.getElementById(id).style.objectFit = 'contain' : 'cover';
+}
