40bad5a21397b859f65d415ad2c23fbaff123686
emilien_letestu <eletestu@gmail.com>
2018-02-19 19:02:08 +0100

    [mod/listener] SecurityListener => refactor

--- src/EventListener/SecurityListener.php
+++ src/EventListener/SecurityListener.php
@@ -8,41 +8,22 @@
 
 namespace App\EventListener;
 
-
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class SecurityListener
 {
     /**
-     * @var SessionInterface
-     */
-    private $session;
-
-
-    /**
-     * AuthenticationListener constructor.
-     * @param SessionInterface $session
-     */
-    public  function __construct(
-        SessionInterface $session
-    )
-    {
-        $this->session = $session;
-    }
-
-    /**
      * @param InteractiveLoginEvent $event
      */
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
     {
-        $user = $event->getAuthenticationToken()->getUser();
-
-        $this->session->getFlashBag()
-            ->add(
-                'success',
-                'Boujour ' .$user->getName().' '.$user->getSurname().' content de vous revoir !'
-            )
+        return
+            $event->getRequest()
+                ->getSession()
+                ->getFlashBag()
+                ->add('success',
+                      'Boujour ' . $event->getAuthenticationToken()->getUser()->getName().' '.$event->getAuthenticationToken()->getUser()->getSurname().' content de vous revoir !'
+                )
         ;
     }
 }
