68dfdf383f91836ec06d4b66bb2e507fb0a37db3
emilien_letestu <eletestu@gmail.com>
2018-02-20 15:21:03 +0100

    [mod/js] terms => bind article status with article visibility

--- public/js/terms.js
+++ public/js/terms.js
@@ -17,13 +17,28 @@ function showAnchor(anchor) {
  * @returns {string}
  */
 function showHideArticle(title) {
-
    var articleNum = title.innerHTML.match(/\d+/);
    var article    = $class("termArticle")[articleNum - 1];
    var style = getStyle(article,"display");
+
+   updateArticleIcon(style,articleNum);
 
    return style === "none" ?
        article.style.display = "block" :
        article.style.display = "none"
    ;
+}
+
+/**
+ * @param style
+ * @param articleNum
+ * @returns {*}
+ */
+function updateArticleIcon(style, articleNum) {
+
+   var icon = $class("articleIcon")[articleNum - 1];
+   return style === "none" ?
+        swappClass(icon,"fa-caret-right","fa-caret-down"):
+        swappClass(icon,"fa-caret-down","fa-caret-right")
+    ;
 }
