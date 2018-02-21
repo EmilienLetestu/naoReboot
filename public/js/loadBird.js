51cd60044a08a640a2f78851b3beac63e47bd139
emilien_letestu <eletestu@gmail.com>
2018-02-21 10:28:23 +0100

    [mod/js] loadBird.js => show/hide map

--- public/js/loadBird.js
+++ public/js/loadBird.js
@@ -4,3 +4,21 @@
 window.addEventListener('load', function () {
     loadElement(3,'birdInfo','bird');
 });
+
+/**
+ * @param id
+ * @param trigger
+ */
+function showHideBirdMap(id,trigger) {
+    var style = getStyle($id(id),'display');
+
+    style === 'none' ?
+        $id(id).style.display = "block" :
+        $id(id).style.display = "none"
+    ;
+
+    style === 'none' ?
+        swappClass(trigger,'fa-plus','fa-minus'):
+        swappClass(trigger,'fa-minus','fa-plus')
+    ;
+}
