14f935839176caa82c1919219cb0eb53290fc189
emilien_letestu <eletestu@gmail.com>
2018-02-27 00:24:40 +0100

    [add/js] perform search via ajax

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -12,12 +12,36 @@ window.addEventListener('scroll',function () {
 
 
 
+
 function searchWidth() {
     $id('search').style.width = "600px";
-    $id('nav_search_submit').style.left="49%";
+    $id('nav_search_submit').style.left="47%";
 }
 
 function searchBlur() {
     $id('search').style.width = "inherit";
     $id('nav_search_submit').style.left="24%";
+}
+
+/**
+ * @param event
+ */
+function search(event){
+    event.preventDefault();
+
+    if($id("nav_search_search").value !== ""){
+
+        var form = $id("searchForm");
+        var url = "/resultat-de-la-recherche";
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if(this.readyState === 4){
+
+                generateMsg('jsGenerated','jsGeneratedMsg',this.responseText,'#5a5a5a');
+            }
+        };
+        xmlhttp.open("POST",url,true);
+        xmlhttp.send(new FormData(form));
+    }
 }
