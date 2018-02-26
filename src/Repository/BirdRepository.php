eadf31c9db2ffd8d12831afd487e1e26535315d6
emilien_letestu <eletestu@gmail.com>
2018-02-26 17:45:14 +0100

    [mod/repository] bird => extend search to location

--- src/Repository/BirdRepository.php
+++ src/Repository/BirdRepository.php
@@ -18,12 +18,13 @@ class BirdRepository extends EntityRepository
     {
         $queryBuilder = $this->createQueryBuilder('b');
         $queryBuilder
+            ->innerJoin('b.reports', 'r')
             ->andWhere('b.id LIKE :search
                 OR b.speciesFr LIKE :search
                 or b.speciesLatin LIKE :search
                 OR b.breed LIKE :search
-                OR b.birdGroup LIKE :search')
-            ->innerJoin('b.reports', 'r')
+                OR b.birdGroup LIKE :search
+                OR r.location Like :search')
             ->andWhere('r.validated = 1')
             ->addSelect('r')
             ->setParameter('search', '%'.$search.'%')
