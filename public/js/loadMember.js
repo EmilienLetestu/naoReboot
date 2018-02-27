c71360cfc5b183deadc87c7cb2d9f6e511ae6cf3
emilien_letestu <eletestu@gmail.com>
2018-02-27 11:10:32 +0100

    [add/js/template] adminMember => fully functionnal filter system

--- public/js/loadMember.js
+++ public/js/loadMember.js
@@ -3,6 +3,28 @@
  */
 
 window.addEventListener('load', function () {
-    loadElement(6,'memberInfo','member');
-    removeLoadBtn('memberLoadBtn','memberInfo',6);
+    loadElement(4,'memberInfo','member');
+    removeLoadBtn('memberLoadBtn','memberInfo',4);
 });
+
+function memberFilter() {
+    var select = $id("memberFilter");
+    var option = select.options[select.selectedIndex].value;
+    select.options[0].innerHTML = "retirer les filtres";
+    var classes = $class("memberInfo");
+    var limit = !$id("memberLoadBtn") ? classes.length : $id("memberLoadBtn").value;
+
+    for (var i = 0; i < limit; i++) {
+
+        if(getStyle(classes[i],"display") === "block"){
+            classes[i].classList.contains(option) ?
+                classes[i].style.display = "block" :
+                classes[i].style.display = "none"
+            ;
+        }
+
+        if(option === " "){
+            classes[i].style.display = "block";
+        }
+    }
+}
