739d67af6da41e266bfd93682854a750f0b2ba90
emilien_letestu <eletestu@gmail.com>
2018-03-05 17:31:31 +0100

    [add/template/js]browseReport => filter reset btn

--- public/js/loadReport.js
+++ public/js/loadReport.js
@@ -5,4 +5,15 @@
 window.addEventListener('load', function () {
     loadElement(6,'card','reportCard');
     removeLoadBtn('browserLoadBtn','card',6);
+    showResetBtn();
 });
+
+function showResetBtn(){
+    var order = $id('filter_order');
+    var bird  = $id('filter_bird');
+
+    if(order.options[order.selectedIndex].value !== "" || bird.options[bird.selectedIndex].value !== ""){
+        $id('resetFilter').style.display = screen.width <= 768 ? "block" : "inline-block";
+    }
+
+}
