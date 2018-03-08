2ac7fee5bbeee32b61a3ea163330d7ca6255cfe5
emilien_letestu <eletestu@gmail.com>
2018-03-08 23:32:39 +0100

    [js] fix bug on form reset

--- public/js/loadReport.js
+++ public/js/loadReport.js
@@ -5,12 +5,21 @@
 window.addEventListener('load', function () {
     loadElement(6,'card','reportCard');
     removeLoadBtn('browserLoadBtn','card',6);
-    showResetBtn();
+
+    if(window.location.pathname === "/observations/en-attente-de-validation" ){
+        showResetBtn("unvalidated_filter_order","unvalidated_filter_bird");
+    }
+
+    if(window.location.pathname === "/observations/valide" ){
+        showResetBtn("filter_order","filter_bird");
+    }
 });
 
-function showResetBtn(){
-    var order = $id('filter_order');
-    var bird  = $id('filter_bird');
+
+function showResetBtn(filter1, filter2){
+
+    var order = $id(filter1);
+    var bird  = $id(filter2);
 
     if(order.options[order.selectedIndex].value !== "" || bird.options[bird.selectedIndex].value !== ""){
         $id('resetFilter').style.display = screen.width <= 768 ? "block" : "inline-block";
