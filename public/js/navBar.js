b2c827528ad3d74df606caff47d3e396705c13dc
emilien_letestu <eletestu@gmail.com>
2018-03-11 11:14:05 +0100

    !! delete all php/ajax based search functions !!

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -9,13 +9,6 @@ window.addEventListener('scroll',function () {
     }
 });
 
-
-document.getElementById('nav_search_search').addEventListener('keyup',function (event) {
-    search(event);
-});
-
-
-
 if(screen.width > 768){
     document.getElementById('search').addEventListener('mouseenter',function () {
         searchWidth();
@@ -53,32 +46,4 @@ function mobileSearch(style) {
 
 function searchBlur() {
     $id('search').style.width = "inherit";
-}
-
-
-/**
- * @param event
- */
-function search(event){
-
-    event.preventDefault();
-
-    if($id("nav_search_search").value === ""){
-        $id('jsGenerated').style.display = "none";
-    }
-
-    if($id("nav_search_search").value !== ""){
-
-        var form = $id("searchForm");
-        var url = "/resultat-de-la-recherche";
-        var xmlhttp = new XMLHttpRequest();
-
-        xmlhttp.onreadystatechange = function () {
-            if(this.readyState === 4){
-                generateMsg('jsGenerated','jsGeneratedMsg',this.responseText,'#5a5a5a');
-            }
-        };
-        xmlhttp.open("POST",url,true);
-        xmlhttp.send(new FormData(form));
-    }
 }
