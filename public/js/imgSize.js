bf885c9d4d52683e8bfef565b9061652959e3f04
emilien_letestu <eletestu@gmail.com>
2018-03-21 15:34:44 +0100

    [js] remove evalSize() for default image

--- public/js/imgSize.js
+++ public/js/imgSize.js
@@ -7,6 +7,8 @@
  * @returns {string}
  */
 function evalSize(id) {
+
     var ratio = document.getElementById(id).naturalWidth/document.getElementById(id).naturalHeight;
+
     return ratio < 1.1 ? document.getElementById(id).style.objectFit = 'contain' : 'cover';
 }
